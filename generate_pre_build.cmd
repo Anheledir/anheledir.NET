b0f760b8ebefa853459a6d66fbf44ebaf6bd3f70
Gordon Breuer <mail@gordon-breuer.de>
2016-03-07 00:18:40 +0100

    add category pages generation to pre build script

--- generate_pre_build.cmd
+++ generate_pre_build.cmd
@@ -1,5 +1,10 @@
 @echo off
 cd %DEPLOYMENT_SOURCE%
+
 ruby generate-tags
 echo Clean-up old tag pages (%DEPLOYMENT_TARGET%\tags\*.html) ...
 del %DEPLOYMENT_TARGET%\tags\*.html
+
+ruby generate-categories
+echo Clean-up old category pages (%DEPLOYMENT_TARGET%\categories\*.html) ...
+del %DEPLOYMENT_TARGET%\categories\*.html
