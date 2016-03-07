d42a99d464d999a726cb014fb82a8b22e77e237a
Gordon Breuer <mail@gordon-breuer.de>
2016-03-07 01:22:51 +0100

    fixed pre build script

--- generate_pre_build.cmd
+++ generate_pre_build.cmd
@@ -1,10 +1,10 @@
 @echo off
 cd %DEPLOYMENT_SOURCE%
 
+echo Clean-up old tag pages (%DEPLOYMENT_SOURCE%\tags\*.html) ...
+del %DEPLOYMENT_SOURCE%\tags\*.html
 ruby generate-tags
-echo Clean-up old tag pages (%DEPLOYMENT_TARGET%\tags\*.html) ...
-del %DEPLOYMENT_TARGET%\tags\*.html
 
+echo Clean-up old category pages (%DEPLOYMENT_SOURCE%\categories\*.html) ...
+del %DEPLOYMENT_SOURCE%\categories\*.html
 ruby generate-categories
-echo Clean-up old category pages (%DEPLOYMENT_TARGET%\categories\*.html) ...
-del %DEPLOYMENT_TARGET%\categories\*.html
